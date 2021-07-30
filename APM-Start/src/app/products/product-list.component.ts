4af3775988a1a5a60c590ef179da6f115a1f6912
Junior <junior.jaava@gmail.com>
2021-07-30 18:34:04 +0200

    4.3 - ViewChild and ViewChildren - accessing native html element

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 import { IProduct } from './product';
 import { ProductService } from './product.service';
@@ -7,13 +7,15 @@ import { ProductService } from './product.service';
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, AfterViewInit {
     pageTitle: string = 'Product List';
     showImage: boolean;
 
     imageWidth: number = 50;
     imageMargin: number = 2;
     errorMessage: string;
+
+    @ViewChild('filterElement') filterElementRef: ElementRef;
 
     private _listFilter: string;
 
@@ -29,7 +31,13 @@ export class ProductListComponent implements OnInit {
     filteredProducts: IProduct[];
     products: IProduct[];
 
-    constructor(private productService: ProductService) { }
+    constructor(private productService: ProductService) { 
+
+    }
+
+    ngAfterViewInit(): void {
+        this.filterElementRef.nativeElement.focus();
+    }
 
     ngOnInit(): void {
         this.productService.getProducts().subscribe(
