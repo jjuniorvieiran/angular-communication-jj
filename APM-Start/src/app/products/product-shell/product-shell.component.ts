cd4f06ee25c64c1a60e142f9c9cd1ed345dba4ee
Junior <junior.jaava@gmail.com>
2021-09-19 19:24:18 +0200

    8. Communicating through service notifications - One more component subscribed to select product subject

--- APM-Start/src/app/products/product-shell/product-shell.component.ts
+++ APM-Start/src/app/products/product-shell/product-shell.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { ProductService } from '../product.service';
 
 @Component({
     templateUrl: './product-shell.component.html'
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
 export class ProductShellComponent implements OnInit {
     pageTitle: string = 'Products';
     monthCount: number;
+    sub: Subscription;
 
-    constructor() { }
+    constructor(private productService: ProductService) { }
 
     ngOnInit() {
+        this.sub = this.productService.selectedProductChanges$.subscribe(selectedProduct => {
+            if (selectedProduct) {
+                const start = new Date(selectedProduct.releaseDate);
+                const now = new Date();
+                this.monthCount = now.getMonth() - start.getMonth()
+                    + (12 * (now.getFullYear() - start.getFullYear()));
+            } else {
+                this.monthCount = 0;
+            }
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.sub.unsubscribe();
     }
 
 }
